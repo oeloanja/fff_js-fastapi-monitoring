0222366bd3fa02f5dae841e4d81264fe67b2405e
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-12-02 17:35:46 +0900

    add

--- app/schemas.py
+++ app/schemas.py
@@ -28,67 +28,66 @@ feature_names = [
 
 class loan(BaseModel):
     loan_amnt : float = Field(
-        ..., ge=0 , description = 'The amount of money requested for the loan.'
+        ..., ge=0 , description = "The amount of money requested for the loan."
     )
     int_rate : float = Field(
-        ..., ge=0 , description = 'The interest rate on the loan.'
+        ..., ge=0 , description = "The interest rate on the loan."
     )
     installment : float = Field(
-        ..., ge=0 , description = ' The monthly payment amount for the loan.'
+        ..., ge=0 , description = "The monthly payment amount for the loan."
     )
     grade : float = Field(
-        ..., ge=0 , description = 'The credit grade assigned to the loan.'
+        ..., ge=0 , description = "The credit grade assigned to the loan."
       
     )
     emp_length : float = Field(
-        ..., ge=0 , description = 'The length of the borrower’s employment in years.'
-
+        ..., ge=0 , description = "The length of the borrower’s employment in years."
     )
     annual_inc : float = Field(
-        ..., ge=0 , description = 'The borrower’s annual income.'
+        ..., ge=0 , description = "The borrower’s annual income."
     )
     dti : float = Field(
-        ..., ge=0 , description = 'Debt-to-income ratio, calculated as the borrower’s monthly debt payments divided by their monthly income.'
+        ..., ge=0 , description = "Debt-to-income ratio, calculated as the borrower’s monthly debt payments divided by their monthly income."
     )
     loan_amnt : float = Field(
-        ..., ge=0 , description = ''
+        ..., ge=0 , description = "why"
     )
     open_acc : float = Field(
-        ..., ge=0 , description = 'The number of open credit lines in the borrower’s credit history.'
+        ..., ge=0 , description = "The number of open credit lines in the borrower’s credit history."
     )
     pub_rec : float = Field(
-        ..., ge=0 , description = 'The number of derogatory public records in the borrower’s credit history.'
+        ..., ge=0 , description = "The number of derogatory public records in the borrower’s credit history."
     )
     revol_bal : float = Field(
-        ..., ge=0 , description = 'The total credit revolving balance.'
+        ..., ge=0 , description = "The total credit revolving balance."
     )
     revol_util : float = Field(
-        ..., ge=0 , description = 'The revolving line utilization rate, or the amount of credit used relative to the total available credit.'
+        ..., ge=0 , description = "The revolving line utilization rate, or the amount of credit used relative to the total available credit."
     )
     total_acc : float = Field(
-        ..., ge=0 , description = 'The total number of credit accounts the borrower has.'
+        ..., ge=0 , description = "The total number of credit accounts the borrower has."
     )
     collections_12_mths_ex_med : float = Field(
-        ..., ge=0 , description = ' The number of collections in the past 12 months, excluding medical collections.'
+        ..., ge=0 , description = "The number of collections in the past 12 months, excluding medical collections."
     )
     
     mort_acc : float = Field(
-        ..., ge=0 , description = 'The number of mortgage accounts.'
+        ..., ge=0 , description = "The number of mortgage accounts."
     )
     emp : float = Field(
-        ..., ge=0 , description = ' The borrower’s employer.'
+        ..., ge=0 , description = "The borrower’s employer."
     )
     home_ownership : float = Field(
-        ..., ge=0 , description = 'The borrower’s home ownership status (e.g., rent, own, mortgage).'
+        ..., ge=0 , description = "The borrower’s home ownership status (e.g., rent, own, mortgage)."
     )
     #purpose : float = Field(
     #    ..., ge=0 , description = ' The purpose of the loan (e.g., debt consolidation, home improvement).'
     #)
     earliest_cr_line : float = Field(
-        ..., ge=0 , description = 'The date when the borrower’s earliest credit line was opened.'
+        ..., ge=0 , description = "The date when the borrower’s earliest credit line was opened."
     )
     issue_d : float = Field(
-        ..., ge=0 , description = 'The date when the loan was issued.'
+        ..., ge=0 , description = "The date when the loan was issued."
     )
 
 class Rating(BaseModel):
@@ -96,5 +95,5 @@ class Rating(BaseModel):
         ...,
         ge = 0 ,
         le = 10,
-        description = 'The current status of the loan (e.g., fully paid, charged off).' 
+        description = "The current status of the loan (e.g., fully paid, charged off)."
     )
